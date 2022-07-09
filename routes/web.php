8d349b30402016b2fbcc54b5a874a30b01c5df98
Ridwan Satria Wicaksana <wicaksana.rs@gmail.com>
2022-07-09 21:52:12 +0700

    fifth commit

--- routes/web.php
+++ routes/web.php
@@ -86,4 +86,4 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
-Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('auth');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
